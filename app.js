add99b3a442d7aee29781e7b754487aa7fa106f0
Rod Doe <doerodney@gmail.com>
2022-01-07 17:53:46 -0800

    Initial hack effort.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const books = [
+  {
+    "title": "Moby Dick",
+    "author": "Herman Melville",
+    "yearPublished": 1851
+
+  }
+];
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
@@ -9,5 +17,78 @@ app.get("/health", (req, res) => {
 });
 
 
+/*
+curl --data '{"author": "Douglas Adams", "title": "The Hitchhikers Guide to the Galaxy", "yearPublished": 1979}' --header "Content-Type: application/json" --request POST http://localhost:5000/api/books
+*/
+app.post("/api/books", (req, res) => {
+  const id = books.length + 1
+  req["id"] = id
+  books.push(req)
+  res.status(201).send()
+
+/*
+curl --data '{"author": "Douglas Adams", "title": "The Hitchhikers Guide to the Galaxy", "yearPublished": 1979}' --header "Content-Type: application/json" --request POST http://localhost:5000/api/books
+
+HTTP Status: 201 Created
+Request Body:
+{
+  "author": "Douglas Adams",
+  "title": "The Hitchhiker's Guide to the Galaxy",
+  "yearPublished": 1979
+}
+
+Response Body:
+{
+  "id": 1,
+  "author": "Douglas Adams",
+  "title": "The Hitchhiker's Guide to the Galaxy",
+  "yearPublished": 1979
+}
+*/
+});
+
+
+/* 
+curl --request GET http://localhost:5000/api/books
+*/
+app.get("/api/books", (req, res) => {
+  res.status(200).send(books)
+/*
+  HTTP Status: 200 OK
+  
+  Response Body:
+  {
+    "books": [
+      {
+        "id": ,
+        "author": "Philip K. Dick",
+        "title": "Do Androids Dream of Electric Sheep?",
+        "yearPublished": 1968
+      },
+      {
+        "id": 1,
+        "author": "Douglas Adams",
+        "title": "The Hitchhiker's Guide to the Galaxy",
+        "yearPublished": 1979
+      },
+      {
+        "id": 3,
+        "author": "William Gibson",
+        "title": "Neuromancer",
+        "yearPublished": 1984
+      }
+    ]
+*/
+});
+
+// curl --request DELETE http://localhost:5000/api/books
+app.delete("/api/books", (req, res) => {
+  /*
+  HTTP Status: 204 No Content
+
+  Response Body: None
+  */
+  res.status(204).send()
+});
 
 module.exports = app;
