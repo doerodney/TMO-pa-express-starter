1fabc1b67cc235e0f84686d29e0224d3f74dee09
Rod Doe <doerodney@gmail.com>
2022-01-10 20:33:36 -0800

    Moved files to server directory.  Unit test works but incomplete.

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../app");
+const app = require("../server/app");
 
 const { expect } = chai;
 chai.use(chaiHttp);
@@ -17,6 +17,41 @@ describe("Base routes", () => {
   });
 });
 
-describe("Additional Routes", () => {
-
+describe("/POST book", () => {
+  it('it should post a book with no id but return a book with an id', (done) => {
+    let book = {
+      "title": "Eloquent JavaScript",
+      "author": "Marijn Haverbeke",
+      "yearPublished": 2019
+    }
+    chai.request(app)
+      .post('/api/books')
+      .send(book)
+      .end((err, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.have.property('id');
+          expect(res.body).to.have.property('author');
+          expect(res.body).to.have.property('title');
+          expect(res.body).to.have.property('yearPublished');
+        done()
+      })
+  })
 });
+
+
+describe("/DELETE books", () => {
+  it('it should delete all books', (done) => {
+    chai.request(app)
+      .delete('/api/books')
+      .end((err, res) => {
+          expect(res).to.have.status(204);
+        done()
+      })
+  })
+});
+
+/*
+{"author": "Douglas Adams", "title": "The Hitchhikers Guide to the Galaxy", "yearPublished": 1979}
+{"title": "Moby Dick", "author": "Herman Melville", "yearPublished": 1851}
+{"author": "Philip K. Dick", "title": "Do Androids Dream of Electric Sheep?", "yearPublished": 1968}
+*/
